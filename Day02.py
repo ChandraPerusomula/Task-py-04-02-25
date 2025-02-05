13031c6f9e46f3156fbe5ed64698dfe7693b172d
chandra perusomula <chandra.perusomula@gmail.com>
2025-02-05 11:48:52 +0530

    Add files via upload

---
+++ Day02.py
@@ -0,0 +1,59 @@
+# How to print...?
+print('chandra')
+print(0)
+print(8618129071)
+print('wel come to my word')
+print(420)
+print('Once money started speaking, no language in the world could respond to it.')
+print(999)
+
+
+#performing a operation with out using a variable
+print(999+999)
+print(456987321+987321456)
+print(909-9990)
+print(456987321-987321456)
+print(6*6)
+print(45689*3694)
+print(25%5)
+print(35%3)
+
+# with using a variable
+am01=46
+am02=25
+print(am01+am02)
+print(am01-am02)
+print(am01%am02)
+print(am01*am02)
+
+
+# Integer
+a=1
+b=-22
+print(type(a),type(b))
+
+# strings
+str='chandra'
+print(str[0])
+print(str[3])
+print(str[1])
+print(str[5])
+print(str[-2])
+print(str[-5])
+
+# slicing
+print(len(str))
+print(str[2:6])
+print(str[-2:-3])
+print(str[:])
+print(str[-2:-6])
+print(str[2:4:2])
+print(str[-4:-1])
+
+#indexing
+print(str[0])
+print(str[2])
+print(str[1])
+print(str[4])
+print(str[3])
+print(str[-3:-1])
